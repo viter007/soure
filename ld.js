b22e2c7ab3694bfe778d56ad9408ed06a77a7cd3
viter007 <96801860+viter007@users.noreply.github.com>
2021-12-29 10:20:05 +0800

    Delete ld.js

--- ld.js
+++
@@ -1,4 +0,0 @@
-document.body.innerHTML += ('<div id="loader-wrapper"><div id="loader"></div><div class="loader-section section-left"></div><div class="loader-section section-right"></div><div id="load_title"></div></div>');
-window.onload = function () {
-    document.body.className += ' loaded';
-}
